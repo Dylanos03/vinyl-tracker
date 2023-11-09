bd595a5ad52e9c9f2bbfba902294b674f6c03b87
Dylanos03 <dylan2.osullivan@live.uwe.ac.uk>
2023-11-09 12:16:22 +0000

    working db

---
+++ src/components/tuneCard.tsx
@@ -0,0 +1,19 @@
+import type { tune } from "@prisma/client";
+
+function TuneCard(props: tune) {
+  return (
+    <div className="rounded-md bg-zinc-700 p-4 text-white">
+      <h2 className="text-2xl">{props.id}</h2>
+      <h3 className="text-3xl font-bold">
+        {props.tuneName} -{" "}
+        <span className="font-normal">{props.tuneArtist}</span>
+      </h3>
+      <div>
+        <h3>BPM: {props.bpm}</h3>
+        <h3>Key: {props.tuneKey}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default TuneCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,42 @@
-import Head from "next/head";
-import Link from "next/link";
+import { FormEvent, useState } from "react";
+import TuneCard from "~/components/tuneCard";
 
 import { api } from "~/utils/api";
 
 export default function Home() {
+  const [name, setName] = useState("");
   const hello = api.post.hello.useQuery({ text: "from tRPC" });
+  const tunes = api.tunes.getAll.useQuery();
+  const { mutate } = api.post.create.useMutation();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    mutate({ name: name });
+    setName("");
+  };
 
   return (
     <>
-      <main>Hello</main>
+      <main className="bg-zinc-900 p-8 lg:p-12">
+        <h1 className="flex justify-center font-mono text-3xl font-bold text-white">
+          Your Collection
+        </h1>
+        <div className="flex">
+          {tunes.data?.map((item) => {
+            return <TuneCard {...item} />;
+          })}
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            id="nameInput"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="bg-zinc-700 text-white"
+          />
+        </form>
+      </main>
     </>
   );
 }
